b4a66de5ce998364ae44757dfaa0615f88a7f3db
RiccardoMarroni <marroni.riccardo@gmail.com>
2024-06-12 09:36:53 +0200

    fixed api

--- routes/api.php
+++ routes/api.php
@@ -2,12 +2,11 @@
 
 use App\Http\Controllers\Api\ProjectController;
 use App\Http\Controllers\Admin\TechnologyControllerController;
-use App\Http\Controllers\Mail\PortfolioContactController;
 use App\Models\Technology;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Mail;
+
 use Illuminate\Support\Facades\Route;
-use App\Mail\PortfolioContact;
+
 use App\Models\Contact;
 
 /*
@@ -29,13 +28,5 @@ Route::get('projects', [ProjectController::class, 'index']);
 // Route::get('tecnology', [TechnologyController::class, 'index']);
 Route::get('projects/{project:slug}', [ProjectController::class, 'show']);
 
-// remember to remove before the productio phase
-Route::get('mailable', function () {
 
-    $data = Contact::all()->last();
-    // dd($data);
 
-    return new App\Mail\PortfolioContact($data);
-});
-
-Route::post('contacts', [PortfolioContactController::class, 'store']);
