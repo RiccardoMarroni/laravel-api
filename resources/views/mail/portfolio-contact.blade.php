30a1fd0ba45d7d9a075c231e4dae606c81231b64
RiccardoMarroni <marroni.riccardo@gmail.com>
2024-06-13 10:32:34 +0200

    mail start

--- resources/views/mail/portfolio-contact.blade.php
+++ resources/views/mail/portfolio-contact.blade.php
@@ -1,19 +1,1 @@
-<x-mail::message>
 
-
-
-
-    HI {{ $contact->name }},
-
-    thanks for you're message, il contact you back ASAP!
-
-
-
-
-    {{-- <x-mail::button :url="''">
-        Button Text
-    </x-mail::button> --}}
-
-    Thanks,
-    {{ config('app.name') }}
-</x-mail::message>
